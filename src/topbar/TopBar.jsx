96c66b460f53f40215c3fc7d575640c97807d177
Park Minsoo <pms2259@naver.com>
2024-02-12 14:26:19 +0900

    topList add

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -4,8 +4,18 @@ import './TopBar.css';
 export default function TopBar() {
   return (
       <div className='top'>
-          <div className='topLeft'>l</div>
-          <div className='topCenter'>c</div>
+          <div className='topLeft'>
+            <i class="fab fa-facebook-square"></i>
+          </div>
+      <div className='topCenter'>
+        <ul className='topList'>
+          <li className='topList'>HOME</li>
+          <li className='topList'>ABOUT</li>
+          <li className='topList'>CONTACT</li>
+          <li className='topList'>WRITE</li>
+          <li className='topList'>LOGOUT</li>
+        </ul>
+      </div>
           <div className='topRight'>r</div>
       </div>
   )
