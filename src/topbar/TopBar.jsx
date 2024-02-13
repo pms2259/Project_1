1998e458f6208c4bc567610fed5fe8edc190e435
Park Minsoo <pms2259@naver.com>
2024-02-13 22:44:10 +0900

    topBar Icon, Img add

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -4,19 +4,29 @@ import './TopBar.css';
 export default function TopBar() {
   return (
       <div className='top'>
-          <div className='topLeft'>
-            <i class="fab fa-facebook-square"></i>
-          </div>
-      <div className='topCenter'>
-        <ul className='topList'>
-          <li className='topList'>HOME</li>
-          <li className='topList'>ABOUT</li>
-          <li className='topList'>CONTACT</li>
-          <li className='topList'>WRITE</li>
-          <li className='topList'>LOGOUT</li>
-        </ul>
-      </div>
-          <div className='topRight'>r</div>
+        <div className='topLeft'>
+          <i className="topIcon fa-brands fa-square-facebook"></i>
+          <i className="topIcon fa-brands fa-square-twitter"></i>
+          <i className="topIcon fa-brands fa-square-pinterest"></i>
+          <i className="topIcon fa-brands fa-square-instagram"></i>
+        </div>
+        <div className='topCenter'>
+          <ul className='topList'>
+            <li className='topListItem'>HOME</li>
+            <li className='topListItem'>ABOUT</li>
+            <li className='topListItem'>CONTACT</li>
+            <li className='topListItem'>WRITE</li>
+            <li className='topListItem'>LOGOUT</li>
+          </ul>
+        </div>
+        <div className='topRight'>
+          <img
+            className='topImg'
+            src="../photo_1692162224_75910.jpg"
+            alt=""
+          />
+          <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
       </div>
   )
 }
