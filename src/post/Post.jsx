12f4af780bca77890e9f97f9e9ab3b9488356973
Park Minsoo <pms2259@naver.com>
2024-02-21 22:22:31 +0900

    Post ing~

---
+++ src/post/Post.jsx
@@ -0,0 +1,22 @@
+import "./post.css"
+
+export default function Post() {
+  return (
+      <div className="post">
+          <img
+            className="postImg"
+            src="https://images.pexels.com/photos/947384/pexels-photo-947384.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <div className="postInfo">
+              <div className="postCats">
+                  <span className="postCat">Music</span>
+                  <span className="postCat">Life</span>
+              </div>
+              <span className="postTitle">lorem ipsum dolor sit amet</span>
+              <hr />
+              <span className="postDate">1 hour ago</span>
+          </div>
+      </div>
+  )
+}
