c66b00732fb08c9074d761d7a10bccbe3fbf0395
Park Minsoo <pms2259@naver.com>
2024-02-22 21:03:46 +0900

    post css finish

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -16,7 +16,10 @@ export default function Post() {
               <span className="postTitle">lorem ipsum dolor sit amet</span>
               <hr />
               <span className="postDate">1 hour ago</span>
-          </div>
+      </div>
+      <p className="postDesc">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </p>
       </div>
   )
 }
