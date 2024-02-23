66da3ffdd04dbf9af40e8feae546e9168bb6e712
Park Minsoo <pms2259@naver.com>
2024-02-23 18:28:38 +0900

    single folder create

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 
-import Header from "../../header/Header"
-import Posts from "../../posts/Posts"
-import Sidebar from "../../sidebar/SideBar"
+import Header from "../../components/header/Header"
+import Posts from "../../components/posts/Posts"
+import Sidebar from "../../components/sidebar/SideBar"
 import "./home.css"
 
 export default function Home() {
