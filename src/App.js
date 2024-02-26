71555953ec6f613ccc348c31835ba88ae6f6f590
Park Minsoo <pms2259@naver.com>
2024-02-26 23:05:10 +0900

    SinglePost add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import TopBar from "./components/topbar/TopBar";
-import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 
 function App() {
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,12 @@
-import "./single.css"
+import "./single.css";
+import SideBar from "../sidebar/SideBar";
+import SinglePost from "../../components/singlePost/SinglePost";
 
 export default function Single() {
-  return (
-    <div className="single">
-        single
-    </div>
-  )
+    return (
+        <div className="single">
+            <SinglePost />
+            <SideBar />
+        </div>
+    );
 }
